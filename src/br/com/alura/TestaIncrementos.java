f323eaf95fd631de11b5deff71b1cf970a7e7cf7
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-12-16 22:50:22 -0300

    java

--- src/br/com/alura/TestaIncrementos.java
+++ src/br/com/alura/TestaIncrementos.java
@@ -1,7 +1,28 @@
 package br.com.alura;
 
 public class TestaIncrementos {
+
     public static void main(String[] args) {
-        
+
+        int a = 10;
+        a += ++a + a + ++a;
+
+        System.out.println(a);
+
+
+        int i = 5;
+        System.out.println(i++);
+
+        i += 2;
+        i -= 2;
+        i *= 5;
+        i /= 10;
+        i *= 100;
+        i %= 3;
+        System.out.println(i+=3);
+
+        byte b = 15;
+        b += 200;
+        System.out.println(b);
     }
 }
